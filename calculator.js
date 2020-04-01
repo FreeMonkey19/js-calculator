f733f7a1a4a3f04a582f445fccaa0066de54b764
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-04-01 08:50:55 -0700

    create calculator in javascript - needs refactoring

--- calculator.js
+++ calculator.js
@@ -1,5 +1,39 @@
-const exampleAdditionInput = {
-  num1: 3,
-  num2: 5,
-  operation: 'add',
-}
+const input = {
+  num1: 9,
+  num2: 3,
+  operation: 'divide',
+};
+
+const calculator = function(input) {
+  if (typeof input.num1 != 'number' || typeof input.num2 != 'number') {
+    return 'Error: invalid number entry!';
+  };
+  
+
+  if ((input.operation.toLowerCase() === 'divide' && input.num2 === 0 )|| (input.operation === '/' && input.num2 === 0)) {
+    return 'Error: cannot divide by 0!'
+  };
+  
+ switch (true) {
+   case input.operation.toLowerCase() === 'add' || input.operation === '+': 
+    return input.num1 + input.num2;
+      break;
+    case input.operation.toLowerCase() === 'subtract' || input.operation === '-':
+      return input.num1 - input.num2;
+        break;
+    case input.operation.toLowerCase() === 'multiply' || input.operation === '*':
+      return input.num1 * input.num2;
+        break;
+    case input.operation.toLowerCase() === 'divide' || input.operation === '/':
+      return input.num1 / input.num2;
+        break;
+    default: 
+      return 'Error: invalid operation entry';
+
+ }; // switch end
+
+}; // calculator end
+
+
+
+console.log(calculator(input));
