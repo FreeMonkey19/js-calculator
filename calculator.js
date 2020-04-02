9d6290b2aa3ea77285bb532c8c19dad26aa13580
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-04-01 18:03:43 -0700

    refactor by creating a variable newOperator and condensing switch

--- calculator.js
+++ calculator.js
@@ -1,36 +1,35 @@
 const input = {
-  num1: 9,
-  num2: 3,
-  operation: 'divide',
+  num1: 3,
+  num2: 9,
+  operation: 'ADD',
 };
 
 const calculator = function(input) {
+  // Establish lowerCase for input.operator
+  const newOperator = input['operation'].toLowerCase();
+// check input for valid Number
   if (typeof input.num1 != 'number' || typeof input.num2 != 'number') {
     return 'Error: invalid number entry!';
   };
   
-
-  if ((input.operation.toLowerCase() === 'divide' && input.num2 === 0 )|| (input.operation === '/' && input.num2 === 0)) {
+// check input if divisor is zero - not valid for division
+  if ((newOperator === 'divide' && input.num2 === 0 )|| (newOperator === '/' && input.num2 === 0)) {
     return 'Error: cannot divide by 0!'
   };
   
- switch (true) {
-   case input.operation.toLowerCase() === 'add' || input.operation === '+': 
-    return input.num1 + input.num2;
-      break;
-    case input.operation.toLowerCase() === 'subtract' || input.operation === '-':
+  switch (true) {
+    case newOperator === 'add' || newOperator === '+': 
+      return input.num1 + input.num2;
+    case newOperator === 'subtract' || newOperator === '-':
       return input.num1 - input.num2;
-        break;
-    case input.operation.toLowerCase() === 'multiply' || input.operation === '*':
+    case newOperator === 'multiply' || newOperator === '*':
       return input.num1 * input.num2;
-        break;
-    case input.operation.toLowerCase() === 'divide' || input.operation === '/':
+    case newOperator === 'divide' || newOperator === '/':
       return input.num1 / input.num2;
-        break;
     default: 
       return 'Error: invalid operation entry';
 
- }; // switch end
+  }; // switch end
 
 }; // calculator end
 
